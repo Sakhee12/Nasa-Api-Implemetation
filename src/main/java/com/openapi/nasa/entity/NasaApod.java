71f22e82cf56567aac4fc2d9d30392efd7d549f2
Sakhee12 <sakshidhayatadak@gmail.com>
2025-12-05 09:40:31 +0530

    Initial commit

---
+++ src/main/java/com/openapi/nasa/entity/NasaApod.java
@@ -0,0 +1,129 @@
+package com.openapi.nasa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Entity
+@Builder
+@Table(name="apod")
+public class NasaApod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="copyright")
+    @Size(min=1,message="is required")
+    private String copyright;
+
+    @Column(name="date")
+    @NotNull(message = "is required")
+    @Size(min=1,message = "is required")
+    private String date;
+
+    @Column(columnDefinition="TEXT")
+    @Lob
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String explanation;
+
+    @Column(name="hdurl")
+    @Size(min = 1 , message = "is required")
+    private String hdurl;
+
+    @Column(name="title")
+    @NotNull(message = "is required")
+    @Size(min = 1 , message = "is required")
+    private String title;
+
+    @Column(name="url")
+    @NotNull(message = "is required")
+    @Size(min = 1 , message = "is required")
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public NasaApod() {
+        super();
+    }
+
+    public NasaApod(int id, String copyright, String date, String explanation, String hdurl, String title, String url) {
+        super();
+        this.id = id;
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.title = title;
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "NasaApod [id=" + id + ", copyright=" + copyright + ", date=" + date + ", explanation=" + explanation
+                + ", hdurl=" + hdurl + ", title=" + title + ", url=" + url + "]";
+    }
+
+}
